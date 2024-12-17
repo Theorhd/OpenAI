0ce969099e72f52a49fa3ee15fdc6385d7e468b5
Theo <theo.richard8654@gmail.com>
2024-12-17 10:46:00 +0100

    Refactor chat input handling and streamline message processing in session state

--- main.py
+++ main.py
@@ -5,34 +5,32 @@ import streamlit as st
 API_KEY = os.getenv("api_key")
 client = OpenAI(api_key=API_KEY)
 
-# Initialize session state if not already done
-if 'messages' not in st.session_state:
+if "messages" not in st.session_state:
     st.session_state.messages = []
 
-value = st.chat_input("Prompt")
-
-if value:
-    # Add user message to session state
-    st.session_state.messages.append({"role": "user", "content": value})
-
-    with st.chat_message("user"):
+def new_message(content: str):
+    with (st.chat_message("user")):
+        st.session_state.messages.append({"role": "user", "content": content})
         st.write(value)
+    
+    with (st.chat_message("assistant")):
+        txt = st.header("Waiting for response...")
 
-    with st.chat_message("assistant"):
-        st.header("Waiting for api...")
+        completion = client.chat.completions.create(
+            model = "gpt-4o-mini",
+            messages = [
+                {"role" : "user", "content" : value}
+            ]
+        )
 
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=st.session_state.messages
-    )
+        st.session_state.messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+        txt.text(completion.choices[0].message.content)
 
-    assistant_message = completion.choices[0].message.content
-    # Add assistant message to session state
-    st.session_state.messages.append({"role": "assistant", "content": assistant_message})
-
-    st.markdown(assistant_message)
-
-# Display previous messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
+
+value = st.chat_input("Your message here")
+if (value and value != ""):
+    new_message(value)
+    value = ""
