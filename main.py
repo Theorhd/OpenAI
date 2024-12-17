6a074b9ef266262dc7999735ef45082bd92bcf3e
Theo <theo.richard8654@gmail.com>
2024-12-17 17:59:23 +0100

    Fix

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ if st.button("Clear Chat"):
 
 model = st.selectbox("Choisi ton modèle", ["GPT-4o-mini", "GPT-4o", "GPT 3.5 Turbo", "DALL-E", "GPT-o1-mini", "GPT-o1-preview", "Python Code Expert", "Générateur d'articles"])
 value = st.chat_input("Votre message ici...")
-clear_btn = st.button("Clear Chat")
+st.button("Clear Chat")
 if value and value != "" and model != "Générateur d'articles":
     new_message(value, model)
     value = ""
