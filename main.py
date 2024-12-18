4f0c7f5bbe82d3c30931102c5d6eba5e6eda1884
Theo <theo.richard8654@gmail.com>
2024-12-18 10:50:28 +0100

    undo

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def new_message(content: str, model: str):
         handle_gpt4o_response(content)
     elif model == "GPT-4o-mini":
         handle_chatgpt_response(content)
+    elif model == "GPT-o1-mini":
+        handle_gpto1mini_response(content)
+    elif model == "GPT-o1-preview":
+        handle_gpto1preview_response(content)
     elif model == "GPT 3.5 Turbo":
         handle_gpt35turbo_response(content)
     elif model == "DALL-E":
@@ -82,6 +86,40 @@ def handle_gpt4o_response(content: str):
                 txt.markdown(full_text)
         st.session_state.messages.append({"role": "assistant", "content": full_text})
 
+def handle_gpto1mini_response(content: str):
+    with st.chat_message("assistant"):
+        txt = st.header("Waiting for response...")
+        messages = st.session_state.messages + [{"role": "user", "content": content}]
+        completion = client.chat.completions.create(
+            model="gpt-o1-mini",
+            messages=messages,
+            stream=True
+        )
+        full_text = ""
+        for chunk in completion:
+            if chunk.choices and len(chunk.choices) > 0 and chunk.choices[0].delta.content is not None:
+                chunk_text = chunk.choices[0].delta.content
+                full_text += chunk_text
+                txt.markdown(full_text)
+        st.session_state.messages.append({"role": "assistant", "content": full_text})
+
+def handle_gpto1preview_response(content: str):
+    with st.chat_message("assistant"):
+        txt = st.header("Waiting for response...")
+        messages = st.session_state.messages + [{"role": "user", "content": content}]
+        completion = client.chat.completions.create(
+            model="gpt-o1-preview",
+            messages=messages,
+            stream=True
+        )
+        full_text = ""
+        for chunk in completion:
+            if chunk.choices and len(chunk.choices) > 0 and chunk.choices[0].delta.content is not None:
+                chunk_text = chunk.choices[0].delta.content
+                full_text += chunk_text
+                txt.markdown(full_text)
+        st.session_state.messages.append({"role": "assistant", "content": full_text})
+
 def handle_gpt35turbo_response(content: str):
     with st.chat_message("assistant"):
         txt = st.header("Waiting for response...")
@@ -119,10 +157,11 @@ def generate_article(topic: str):
         messages=[
             {"role": "system", "content": "Tu es un assistant qui rédige des articles. Ta taches est de générer 3 paragraphes par sujet qui te seront demandés."},
             {"role": "user", "content": f"Rédige un article sur le sujet : {topic}"}
-        ]
+        ],
+        stream=True
     )
     article_content = completion.choices[0].message.content
-    st.text(article_content)
+    st.markdown(article_content)
     
     # Generate images
     for _ in range(2):
@@ -139,7 +178,7 @@ value = st.chat_input("Votre message ici...")
 if st.button("Clear Chat"):
     st.session_state.messages = []
 
-model = st.selectbox("Choisi ton modèle", ["GPT-4o-mini", "GPT-4o", "GPT 3.5 Turbo", "DALL-E", "Python Code Expert", "Générateur d'articles"])
+model = st.selectbox("Choisi ton modèle", ["GPT-4o-mini", "GPT-4o", "GPT 3.5 Turbo", "DALL-E", "GPT-o1-mini", "GPT-o1-preview", "Python Code Expert", "Générateur d'articles"])
 
 if value and value != "" and model != "Générateur d'articles":
     new_message(value, model)
